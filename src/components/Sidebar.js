bac5bf09c4171a025123fb16a867558737806a24
Bahadir Colak <bahadircolak34@gmail.com>
2022-04-23 11:12:27 +0300

    third commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,28 +1,8 @@
-import React from 'react';
-import { Container, Nav, NavLink} from 'react-bootstrap';
-import logo from "./images/logo.png";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
 
 function Sidebar() {
   return (
-    <>
-    <Nav className="col-md-12 d-none d-md-block bg-light sidebar" activeKey="/home" onSelect={selectedKey => alert(`selected ${selectedKey}`)}>
-        <img className='logo--img' src={logo}/>
-        <div className="sidebar-sticky"></div>
-            <Nav.Item>
-                <Nav.Link href="/home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Contact</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <NavLink eventKey="link-3">My Skills</NavLink>
-            </Nav.Item>
-            </Nav>
-    </>
+    <div>Sidebar</div>
   )
 }
 
