30bfa2e422806fab64a308331494b9afc1042bef
bahacolak <bahadircolak34@gmail.com>
2022-05-03 17:34:39 +0300

    logos , box-shadow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div className='home'>
-      <h1>Hello It's home page</h1>
+    <div className='home'>     
+      <div className='container'>   
+          <h1>Hey,</h1>
+          <h1>I'm Bahadır,</h1>
+          <h1>Civil Engineer, Web Developer</h1>
+          <p>Front End Developer</p>        
+      <div>
+        <button className='btn'>Contact me!</button>
+      </div>
+      </div>  
     </div>
   )
 }
