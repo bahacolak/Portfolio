88bbe725a658cbdc51be170b6992be75828a08b3
Bahadir Colak <bahadircolak34@gmail.com>
2022-06-27 01:53:58 +0300

    fix

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./Home.css";
 
+
 function Home() {
+  
+
   return (
+    
      <div className='home'>
       <div className='container'>            
-          <h1>Hey, I'm Bahadır Çolak </h1>              
-       <div>
-         <button className='btn'>Contact me!</button>
-       </div>
-       </div>  
+        <h1>Hey, I'm Bahadır Çolak </h1>
+        <p>Front End Developer / Civil Engineer</p>
+        <button className='btn'>Contact me!</button>  
+      </div>  
      </div>
   )
 }
